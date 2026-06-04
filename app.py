da811805f4d9d7744703b32f5cd59608ca5ad95f
bslee1129 <bs7lee@gmail.com>
2026-06-04 13:38:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ if uploaded_file is not None:
             
             # 모델 설정 (하루 50회 미만은 pro / 그 이상 원하면 flash로 변경)
             #model = genai.GenerativeModel(model_name="gemini-2.0-pro")
-            model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+            #model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+
             prompt = (
                 "Analyze the image and extract product information. "
                 "Respond ONLY in JSON format with keys: 'brand', 'product_name', 'barcode', 'ingredients'. "
